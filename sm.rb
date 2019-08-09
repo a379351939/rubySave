5c2cf098fc403bdc126ab708cc6b39fd2bf58a2e
taddyzc <491514218@qq.com>
2019-08-09 12:39:48 +0800

    save

--- sm.rb
+++ sm.rb
@@ -52,11 +52,11 @@ class DFADesign < Struct.new(:start_state, :accept_states, :rulebook)
   end
 end
 
-rulebook = DFARulebook.new([
-  FARule.new(1, 'a', 2), FARule.new(1, 'b', 1),
-  FARule.new(2, 'a', 2), FARule.new(2, 'b', 3),
-  FARule.new(3, 'a', 3), FARule.new(3, 'b', 3)
-])
+# rulebook = DFARulebook.new([
+#   FARule.new(1, 'a', 2), FARule.new(1, 'b', 1),
+#   FARule.new(2, 'a', 2), FARule.new(2, 'b', 3),
+#   FARule.new(3, 'a', 3), FARule.new(3, 'b', 3)
+# ])
 
 require 'set'
 
@@ -72,8 +72,40 @@ class NFARulebook < Struct.new(:rules)
   end
 end
 
+class NFA < Struct.new(:current_states, :accept_states, :rulebook)
+  def accepting?
+    (current_states & accept_states).any?
+  end
+end
+
+class NFA
+  def read_character(character)
+    self.current_states = rulebook.next_states(current_states, character)
+  end
+
+  def read_string(string)
+    string.chars.each do |character|
+      read_character(character)
+    end
+  end
+end
+
+class NFADesign < Struct.new(:start_state, :accept_states, :rulebook)
+  def accepts?(string)
+    to_nfa.tap { |nfa| nfa.read_string(string) }.accepting?
+  end
+
+  def to_nfa
+    NFA.new(Set[start_state],accept_states, rulebook)
+  end
+end
+
 rulebook = NFARulebook.new([
-FARule.new(1, 'a', 1), FARule.new(1, 'b', 1), FARule.new(1, 'b', 2),
-FARule.new(2, 'a', 3), FARule.new(2, 'b', 3),
-FARule.new(3, 'a', 4), FARule.new(3, 'b', 4)
-]) 
+  FARule.new(1, 'a', 1), FARule.new(1, 'b', 1), FARule.new(1, 'b', 2),
+  FARule.new(2, 'a', 3), FARule.new(2, 'b', 3),
+  FARule.new(3, 'a', 4), FARule.new(3, 'b', 4)
+])
+
+nfa_design = NFADesign.new(1, [4], rulebook)
+
+nfa_design.accepts?('bab')
