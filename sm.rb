018ac381c3512dc0021b43307161c74abd586b2c
a379351939 <491514218@qq.com>
2019-08-18 22:38:13 +0800

    save

--- sm.rb
+++ sm.rb
@@ -241,8 +241,25 @@ class Concatenate
     start_state = first_nfa_design.start_state
     accept_states = second_nfa_design.accept_states
     rules = first_nfa_design.rulebook.rules + second_nfa_design.rulebook.rules
-    extra_rules = first_nfa_design.accept_state.map { |state|
+    extra_rules = first_nfa_design.accept_states.map { |state|
       FARule.new(state, nil, second_nfa_design.start_state)
+    }
+    rulebook = NFARulebook.new(rules + extra_rules)
+
+    NFADesign.new(start_state, accept_states, rulebook)
+  end
+end
+
+class Choose
+  def to_nfa_design
+    first_nfa_design = first.to_nfa_design
+    second_nfa_design = second.to_nfa_design
+
+    start_state = Object.new
+    accept_states = first_nfa_design.accept_states + second_nfa_design.accept_states
+    rules = first_nfa_design.rulebook.rules + second_nfa_design.rulebook.rules
+    extra_rules = [first_nfa_design, second_nfa_design].map { |nfa_design|
+      FARule.new(start_state, nil, nfa_design.start_state)
     }
     rulebook = NFARulebook.new(rules + extra_rules)
 
